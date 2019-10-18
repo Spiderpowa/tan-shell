5dac50264d00173b0b548d8af29b247ff3f32764
Jimmy Hu <jim2212001@gmail.com>
2019-10-19 01:51:24 +0800

    add msgId to functions and events

--- test/Shell.js
+++ test/Shell.js
@@ -14,33 +14,36 @@ contract('Shell', function(accounts) {
 
   it("should emit event for stdout", async function() {
     const stream = ['0x01', '0x02', '0x03', '0x04'];
-    const result = await ShellContract.stdout(stream, true, {from: accounts[1]})
+    const result = await ShellContract.stdout(13, stream, true, {from: accounts[1]})
     assert.equal(result.logs.length, 1);
     assert.equal(result.logs[0].event, 'Stdout');
     const args = result.logs[0].args;
     assert.equal(args.clientIndex, 1);
+    assert.equal(args.msgId, 13);
     assert.deepEqual(args.stream, stream);
     assert.equal(args.eof, true);
   });
 
   it("should emit event for stderr", async function() {
     const stream = ['0x01', '0x02', '0x03', '0x04'];
-    const result = await ShellContract.stderr(stream, true, {from: accounts[1]})
+    const result = await ShellContract.stderr(14, stream, true, {from: accounts[1]})
     assert.equal(result.logs.length, 1);
     assert.equal(result.logs[0].event, 'Stderr');
     const args = result.logs[0].args;
     assert.equal(args.clientIndex, 1);
+    assert.equal(args.msgId, 14);
     assert.deepEqual(args.stream, stream);
     assert.equal(args.eof, true);
   });
 
   it("should emit event for stdin", async function() {
     const stream = ['0x01', '0x02', '0x03', '0x04'];
-    const result = await ShellContract.stdin(1, stream, true, {from: accounts[0]})
+    const result = await ShellContract.stdin(1, 12, stream, true, {from: accounts[0]})
     assert.equal(result.logs.length, 1);
     assert.equal(result.logs[0].event, 'Stdin');
     const args = result.logs[0].args;
     assert.equal(args.clientIndex, 1);
+    assert.equal(args.msgId, 12);
     assert.deepEqual(args.stream, stream);
     assert.equal(args.eof, true);
   });
@@ -54,30 +57,30 @@ contract('Shell', function(accounts) {
   it("should reject unregistered client", async function() {
     const stream = ['0x01', '0x02'];
     assert.rejects(() => {
-      return ShellContract.stdout(stream, true, {from:accounts[2]});
+      return ShellContract.stdout(1, stream, true, {from:accounts[2]});
     }, {reason: "unknown client"} );
     assert.rejects(() => {
-      return ShellContract.stderr(stream, true, {from:accounts[2]});
+      return ShellContract.stderr(2, stream, true, {from:accounts[2]});
     }, {reason: "unknown client"} );
   });
 
   it("should reject non owner using stdin", async function() {
     const stream = ['0x01', '0x02'];
     assert.rejects(() => {
-      return ShellContract.stdin(1, stream, true, {from:accounts[1]});
+      return ShellContract.stdin(1, 3, stream, true, {from:accounts[1]});
     }, {reason: "Ownable: caller is not the owner"});
   });
 
   it("should prevent owner inputting unknown client index", async function() {
     const stream = ['0x01', '0x02'];
     assert.rejects(() => {
-      return ShellContract.stdin(0, stream, true, {from:accounts[1]});
+      return ShellContract.stdin(0, 4, stream, true, {from:accounts[1]});
     }, {reason: "invalid client index"});
     assert.rejects(() => {
-      return ShellContract.stdin(-1, stream, true, {from:accounts[1]});
+      return ShellContract.stdin(-1, 5, stream, true, {from:accounts[1]});
     }, {reason: "invalid client index"});
     assert.rejects(() => {
-      return ShellContract.stdin(2, stream, true, {from:accounts[1]});
+      return ShellContract.stdin(2, 6, stream, true, {from:accounts[1]});
     }, {reason: "unknown client"});
   });
 });
